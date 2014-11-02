79a21c05b9f9134668da73174bb8fd7f4fa13b0d
Rikki Guy <git@rikkiguy.me.uk>
2014-11-02 10:03:09 -0500

    It seems to work so far

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <wctype.h>
+#include <string.h>
+#include <wchar.h>
 
 enum value_type {
         VALUE_NULL,
@@ -11,21 +14,26 @@ enum value_type {
         VALUE_LIST
 };
 
-wchar_t a;
-
 typedef struct value value;
 struct value {
         enum value_type type;
-        value* next;
+        value* next; /* The next value in a list */
         
-        union {
-                const wint_t* string;
-                double number;
-                value* list_first;
-        };
+        /* String Type */
+        const wchar_t* string;
+        
+        /* Number Type */
+        double number;
+        
+        /* List Type */
+        value* list_first;
 };
 
-value* alloc_val()
+/* Value management functions */
+value* alloc_val(void);
+void clone_val(value* dest, value* src);
+
+value* alloc_val(void)
 {
         value* val = calloc(1, sizeof(value));
         val->type = VALUE_NULL;
@@ -44,34 +52,34 @@ void clone_val(value* dest, value* src)
                 dest->number = src->number;
                 
         } else if(src->type == VALUE_LIST) {
-                dest->array_first = src->array_first;
+                dest->list_first = src->list_first;
                 
         }
 }
 
 /* Parsing functions */
-int parse_white(const wint_t** code);
-int parse_string(value* val, const wint_t** code);
-int parse_ident(value* val, const wint_t** code);
-int parse_list(value* val, const wint_t** code);
-int parse_number(value* val, const wint_t** code);
-int parse_value(value* val, const wint_t** code);
+int parse_white(const wchar_t** code);
+int parse_string(value* val, const wchar_t** code);
+int parse_ident(value* val, const wchar_t** code);
+int parse_list(value* val, const wchar_t** code);
+int parse_number(value* val, const wchar_t** code);
+int parse_value(value* val, const wchar_t** code);
 
-int parse_white(const wint_t** code)
+int parse_white(const wchar_t** code)
 {
         while( iswspace(**code) ) { ++(*code); }
         return 1;
 }
 
-int parse_string(value* val, const wint_t** code)
+int parse_string(value* val, const wchar_t** code)
 {
-        const wint_t* c = *code;
+        const wchar_t* c = *code;
         
         if(*c != '"') return 0;
         ++c;
         
         /* First we need the length of the string */
-        const wint_t* string_start = c;
+        const wchar_t* string_start = c;
         
         int len = 0;
         while(*c != '"') {
@@ -81,12 +89,13 @@ int parse_string(value* val, const wint_t** code)
         }
         
         /* Allocate a copy */
-        const wint_t* strval = (const wint_t*) calloc(len+1, sizeof(wint_t));
+        wchar_t* strval = (wchar_t*) calloc(len+1, sizeof(wchar_t));
         
         /* Make the copy */
-        while(len > 0) {
-                strval[len] = string_start[len];
-                --len;
+        int i = 0;
+        while(i < len) {
+                strval[i] = string_start[i];
+                ++i;
         }
         
         /* Fast-forward over last " */
@@ -100,27 +109,28 @@ int parse_string(value* val, const wint_t** code)
         return 1;
 }
 
-int parse_ident(value* val, const wint_t** code)
+int parse_ident(value* val, const wchar_t** code)
 {
         /* Must start with alpha character */
         if(!iswalpha(**code)) { return 0; }
         
-        const wint_t* c = *code;
+        const wchar_t* c = *code;
         
         /* Get string length */
         int len = 0;
-        while(!iswspace(*c) && *c != '\0') {
+        while(!iswspace(*c) && *c != '\0' && !iswpunct(*c)) {
                 ++c;
                 ++len;
         }
         
         /* Allocate copy */
-        const wint_t* strval = (const wint_t*) calloc(len+1, sizeof(wint_t));
+        wchar_t* strval = (wchar_t*) calloc(len+1, sizeof(wchar_t));
         
         /* Make the copy */
-        while(len > 0) {
-                strval[len] = code[len];
-                --len;
+        int i = 0;
+        while(i < len) {
+                strval[i] = (*code)[i];
+                ++i;
         }
         
         /* Setup value */
@@ -132,27 +142,27 @@ int parse_ident(value* val, const wint_t** code)
         return 1;
 }
 
-int parse_list(value* val, const wint_t** code)
+int parse_list(value* val, const wchar_t** code)
 {
         /* Must start with [ */
-        if(**code != '[') { return 0; }
+        if(**code != L'[') { return 0; }
         
-        const wint_t* c = *code;
+        const wchar_t* c = *code;
         ++c; /* Skip '[' */
         
         /* Create list */
         value* tval = alloc_val();
         value* tval_start = tval;
         
-        while(*c != ']') {
-                if(*c == '\0') { return 0; } /* Fail if End-of-Code */
+        while(*c != L']') {                
+                if(*c == L'\0') { return 0; } /* Fail if End-of-Code */
                 
                 parse_white(&c);
                 
                 tval->next = alloc_val();
                 tval = tval->next;
                 
-                int result = parse_ident(&c, tval);
+                int result = parse_ident(tval, &c);
                 if(!result) { return 0; }
                 
                 parse_white(&c);
@@ -167,4 +177,72 @@ int parse_list(value* val, const wint_t** code)
         *code = c;
         
         return 1;
+}
+
+void fprintvalue(FILE* file, value* val);
+/* Print a tree of Values */
+void fprintvalue(FILE* file, value* val) {
+        if(val->type == VALUE_NULL) {
+                fprintf(file, "null");
+                
+        } else if(val->type == VALUE_STRING) {
+                fprintf(file, "\"%ls\"", val->string);
+                
+        } else if(val->type == VALUE_LIST) {
+                fprintf(file, "[");
+                value* arrval = val->list_first;
+                while(arrval) {
+                        fprintvalue(file, arrval);
+                        if(arrval->next) { fprintf(file, " "); }
+                        arrval = arrval->next;
+                }
+                fprintf(file, "]");
+                
+        } else {
+                fprintf(file, "%ls", L"«unknown»");
+        }
+}
+
+
+int main(int argc, char** argv)
+{
+        printf("sizeof(wchar_t) = %lu\n", sizeof(wchar_t));
+        
+        FILE* test = fopen("test.sqr", "r");
+        if(test == NULL) {
+                fprintf(stderr, "Couldn't open test.sqr\n");
+                return 0;
+        }
+        
+        fseek(test, 0L, SEEK_END);
+        long int size = ftell(test)+2;
+        fseek(test, 0L, SEEK_SET);
+        
+        wchar_t* program = (wchar_t*) malloc((size+1)*(sizeof(wchar_t)/sizeof(char)));
+        program[size] = L'\0';
+        program[size+1] = WEOF;
+        
+        char* program_raw = (char*) malloc((size+1)*sizeof(char));
+        program_raw[size] = '\0';
+        
+        fread(program_raw, 1, size, test);
+        swprintf(program, size, L"%s", program_raw);
+        
+        fclose(test);
+        free(program_raw);
+        program_raw = NULL;
+        
+        printf("Program: %ls \n", program);
+        
+        value* root_val = alloc_val();
+        const wchar_t* programptr = (const wchar_t*) program;
+        int result = parse_list(root_val, &programptr);
+        
+        if(!result) {
+                fprintf(stderr, "parse failed\n");
+        } else {
+                fprintvalue(stdout, root_val);
+        }
+        
+        return 0;
 }
